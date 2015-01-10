29d7a6a735e47a49eb3081ee91636fa1cbfffecd
gmittal <gautam@mittal.net>
2015-01-09 21:40:58 -0800

    updates, nice json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 */
 
 var express = require('express');
+var request = require('request');
 var app = express();
 
 
@@ -19,10 +20,50 @@ app.use(function(req, res, next) {
 });
 
 app.get('/search/:search_query', function (req, res) {
-	console.log(req.param("search_query").toLowerCase());
-	var id = req.param("search_query").toLowerCase();
+	request('http://suggestqueries.google.com/complete/search?hl=en&ds=yt&client=youtube&hjson=t&q='+ req.param("search_query") +'&cp=1', function (err, response, body) {
+		var suggested_queries = [];
+		for (var i = 0; i < JSON.parse(body)[1].length; i++) {
 
-	res.send('Hello World!');
+			suggested_queries.push(JSON.parse(body)[1][i][0]); // add the suggested query
+		}
+
+		console.log(suggested_queries);
+
+		request('http://gdata.youtube.com/feeds/api/videos?q='+ req.param("search_query") +'&format=5&max-results=1&v=2&alt=jsonc', function (err101, response101, body101) {
+			var gdata = {};
+			gdata["title"] = JSON.parse(body101).data.items[0].title;
+			gdata["description"] = JSON.parse(body101).data.items[0].description;
+			gdata["uploader"] = JSON.parse(body101).data.items[0].uploader;
+			gdata["category"] = JSON.parse(body101).data.items[0].category;
+			gdata["video_id"] = JSON.parse(body101).data.items[0].id;
+			gdata["thumbnails"] = {
+				'small': JSON.parse(body101).data.items[0].thumbnail.sqDefault,
+				'large': JSON.parse(body101).data.items[0].thumbnail.hqDefault
+			};
+			gdata["duration"] = JSON.parse(body101).data.items[0].duration;
+			var prettyDurationMin = Math.floor((JSON.parse(body101).data.items[0].duration)/60);
+			var prettyDurationSeconds = (JSON.parse(body101).data.items[0].duration)%60;
+			gdata["pretty_duration"] = prettyDurationMin.toString() + ":"+ prettyDurationSeconds.toString();
+			// gdata["duration"] = JSON.parse(body101).data.items[0].duration;
+			gdata["views"] = JSON.parse(body101).data.items[0].viewCount;
+			gdata["rate"] = JSON.parse(body101).data.items[0].rating;
+
+				
+
+
+			res.set('Content-Type', 'application/json');
+			res.send(JSON.stringify({'suggested_searches': suggested_queries, 'data': gdata}));
+		});
+
+
+		
+		
+		
+
+	});
+
+
+	
 });
 
 app.get('/id/:youtube_id', function (req, res) {
